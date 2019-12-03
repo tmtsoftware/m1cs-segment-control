c539d0e8dc7bf9c9427b884ecff6d8aa422de56a
smichaels <Zernike1!>
2019-12-02 17:16:25 -0800

    adding command unit test

--- control-assembly/src/main/java/org/tmt/m1cs/controlassembly/JControlAssemblyHandlers.java
+++ control-assembly/src/main/java/org/tmt/m1cs/controlassembly/JControlAssemblyHandlers.java
@@ -103,7 +103,7 @@ public class JControlAssemblyHandlers extends JComponentHandlers {
     public CommandResponse.SubmitResponse onSubmit(ControlCommand controlCommand) {
 
 
-        return new CommandResponse.Error(controlCommand.runId(), "command failed");
+        return new CommandResponse.Completed(controlCommand.runId());
     }
 
     @Override
